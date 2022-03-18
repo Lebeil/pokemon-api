5f1d08ec2ae5244eb7e3f2ace7ab23a09454651f
Liedel <liedel75@gmail.com>
2022-03-18 17:05:34 +0100

    nombre total de pokémons

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/api/pokemons/:id', (req,res)=> {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     res.send(`Vous avez demandé le pokémon ${pokemon.name}`)
+})
 
+app.get('/api/pokemons', (req, res)=> {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment`)
 })
 
 app.listen(port, ()=> console.log(`Notre app node est démarré sur : http://localhost:${port}`))
