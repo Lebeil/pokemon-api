fe9a95b0243c32affcf2b9f3b2a0f7a1da87da4a
Liedel <liedel75@gmail.com>
2022-03-18 16:43:40 +0100

    parseInt express => string

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const port = 3000
 app.get('/', (req, res) => res.send('Hello, express3! 😎'))
 
 app.get('/api/pokemons/:id', (req,res)=> {
-    const id = req.params.id
+    const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Vous avez demandé le pokémon n°${pokemon.name}`)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}`)
 
 })
 
