8767edff1619904fca875e68378d939fb6790b10
Liedel <liedel75@gmail.com>
2022-03-18 18:47:37 +0100

    liste des pokémons

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { success } = require('./helper')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
@@ -9,11 +10,13 @@ app.get('/', (req, res) => res.send('Hello, express3! 😎'))
 app.get('/api/pokemons/:id', (req,res)=> {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Vous avez demandé le pokémon ${pokemon.name}`)
+    const message = 'Un pokemon a bien été trouvé'
+    res.json(success(message, pokemon))
 })
 
 app.get('/api/pokemons', (req, res)=> {
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment`)
+    const message = 'Voici la liste des pokemons'
+    res.json(success(message,pokemons))
 })
 
 app.listen(port, ()=> console.log(`Notre app node est démarré sur : http://localhost:${port}`))
---
+++ helper.js
@@ -0,0 +1,16 @@
+exports.success = (message, data) => {
+    return {
+        message,
+        data
+    }
+}
+
+// méthode détaillée
+// const success = (message, data) => {
+//     return {
+//         message: message,
+//         data: data
+//     }
+// }
+
+// exports.success
