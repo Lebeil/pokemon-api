4fa64b896fad78c7fe737d10ded6b233e65b7ebd
Liedel <liedel75@gmail.com>
2022-03-29 21:31:59 +0200

    fix indentation routes

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require("express")
 const morgan = require("morgan")
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
-const sequelize  = require('sequelize')
+const sequelize = require('./src/db/sequelize')
 
 const app = express()
 const port = 3000
+
+
 
 //middlewares
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(morgan('dev'))
     .use(bodyParser.json())
-    
+// const logger = (req, res, next) => {
+//     console.log(`URL: ${req.url}`);
+//     next()
+// }
+
 sequelize.initDb()
+
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
 
 app.listen(port, ()=> console.log(`Notre app node est démarré sur : http://localhost:${port}`))
