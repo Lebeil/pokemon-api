470f94cfbcfe81c53dfa336459883f6c49cdbaf1
Liedel <liedel75@gmail.com>
2022-04-06 14:31:54 +0200

    add opérateur sequelize

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,31 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
-        Pokemon.findAll()
-        .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
-        })
-        .catch(error => {
-            const message = `La liste des pokémons n'a pas pu être récupérer.`
-            res.status(500).json({ message, data: error })
-        })
+        if(req.query.name){
+            const name = req.query.name
+            return Pokemon.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%` // 'name est le critère de la recherche
+                    }
+                } 
+            })
+            .then(pokemons => {
+                const message = `Il y a ${pokemons.length} pokémons qui correspondent au terme de recherche ${name}.`
+                res.json({message, data: pokemons})
+            })
+        } else {
+            Pokemon.findAll()
+            .then(pokemons => {
+            const message = 'La liste des pokémons a bien été récupérée.'
+            res.json({ message, data: pokemons })
+            })
+            .catch(error => {
+                const message = `La liste des pokémons n'a pas pu être récupérer.`
+                res.status(500).json({ message, data: error })
+            })
+        }   
     })
 }
